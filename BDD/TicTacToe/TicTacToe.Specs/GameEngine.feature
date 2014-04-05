6c5793373d03ad31bd6b31e10ddfcfe34f475872
JamesBender <James@JamesCBender>
2014-04-05 14:31:53 -0700

    with demo from my BDD talk

---
+++ BDD/TicTacToe/TicTacToe.Specs/GameEngine.feature
@@ -0,0 +1,22 @@
+﻿Feature: Tic Tac Toe
+	In order to determine who the winner of the game as
+	As a Player
+	I want to be told who (if anyone) won the game
+
+Scenario: Empty board ends with a tie
+	Given I have a tic tac toe board
+	And the board is empty
+	When I determine the winner
+	Then the result be a " "
+
+Scenario: If top row is all X then X is the winner
+	Given I have a tic tac toe board
+	And "X" is across the top row
+	When I determine the winner
+	Then the result be a "X"
+
+Scenario: If top row is all O then O is the big winner
+	Given I have a tic tac toe board
+	And "O" is across the top row
+	When I determine the winner
+	Then the result be a "O"
---
+++ BDD/TicTacToe/TicTacToe.Specs/TicTacToeSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace TicTacToe.Specs
+{
+    [Binding]
+    public class TicTacToeSteps
+    {
+        private string[,] _board;
+        private TicTacToe.Core.GameEngine _gameEngine = new TicTacToe.Core.GameEngine();
+        private string _result;
+
+        [Given(@"I have a tic tac toe board")]
+        public void GivenIHaveATicTacToeBoard()
+        {
+            _board = new String[3, 3] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
+        }
+
+        [Given(@"the board is empty")]
+        public void GivenTheBoardIsEmpty()
+        {
+            return;
+        }
+
+        [When(@"I determine the winner")]
+        public void WhenIDetermineTheWinner()
+        {
+            _result = _gameEngine.GetWinner(_board);
+        }
+
+        [Then(@"the result be a ""(.*)""")]
+        public void ThenTheResultBeA(string expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _result);
+        }
+
+        [Given(@"""(.*)"" is across the top row")]
+        public void GivenIsAcrossTheTopRow(string token)
+        {
+            _board[0, 0] = token;
+            _board[0, 1] = token;
+            _board[0, 2] = token;
+        }
+
+    }
+}
